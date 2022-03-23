279417760a56a5f3a898f25f35a089e469ced4ce
Bruno Cabete <bpcabete@gmail.com>
2022-03-23 12:39:58 -0300

    scrollbar and down arrow

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ const eachSlideAlt = img =>  /*html*/`<div class="general-container split" id="$
 </div>
 </div>`
 
-const finalSlide = () =>  /*html*/`<div class="general-container final" id="">
+const finalSlide = () =>  /*html*/`<div class="general-container final" id="final">
 <div class="general-container-inner ">
   <div class="bg" >
   </div>
@@ -98,11 +98,9 @@ const todosOsSlides = document.querySelectorAll('.general-container')
 const deadzone = parseInt(getComputedStyle(document.body).getPropertyValue('--deadzone').replace('px', ''))
 const base = {}
 const screensize = window.innerWidth >= 768 ? window.innerHeight : document.querySelector('.cover').offsetHeight
-
+const descriptionDeadzone = window.innerWidth >= 768 ? 300 : 175
 
 for (let i = 1; i <= slideCount; i++) {
-
-
   base[i] = (screensize + deadzone) * (i - 1) + (window.innerHeight)
 }
 
@@ -111,7 +109,7 @@ document.addEventListener('scroll', () => {
 
     const id = v.getAttribute('id')
     const offset = v.offsetTop
-    const descriptionDeadzone = window.innerWidth >= 768 ? 300 : 175
+
 
     if (offset > base[id] + descriptionDeadzone) {
       try {
@@ -129,4 +127,48 @@ document.addEventListener('scroll', () => {
       }
     }
   })
+
+  document.querySelector('#progressbar .pointer').style.top = `${(window.scrollY / document.body.clientHeight * 100).toFixed(2)}%`
+  console.log()
 })
+
+
+document.getElementById('down-arrow').addEventListener('click', (ev) => {
+  const eachSlideOffset = []
+  document.querySelectorAll('.general-container').forEach((v) => {
+    if (v.offsetTop > window.scrollY) {
+      eachSlideOffset.push(v.offsetTop)
+      eachSlideOffset.push(v.offsetTop + descriptionDeadzone + 10)
+    }
+  })
+  eachSlideOffset.sort(function (a, b) { return a - b })
+  console.log(eachSlideOffset)
+  window.scroll({
+    top: eachSlideOffset[1],
+    left: 0,
+    behavior: 'smooth'
+  })
+})
+
+
+const eachslidePosition = []
+document.querySelectorAll('.general-container').forEach((v) => {
+  // eachslidePosition.push(v.offsetTop)
+  eachslidePosition.push(v.offsetTop + descriptionDeadzone)
+})
+
+const markersPositions = eachslidePosition.map((v) => {
+  return (v / document.body.clientHeight * 100).toFixed(2) + '%'
+})
+
+console.log(markersPositions)
+
+markersPositions.forEach((v) => {
+
+  let marker = document.createElement('div')
+  marker.classList.add('marker')
+  marker.style.top = v
+  document.querySelector('#progressbar .bar').appendChild(marker)
+}
+
+)
