0a3c197b38826dc22a28248fd41621da425e5642
Bruno Cabete <bpcabete@gmail.com>
2022-04-14 11:42:05 -0300

    final project

--- main.js
+++ main.js
@@ -1,3 +1,77 @@
+const conteudo = {
+  1: ['ARAUCÁRIA',
+   /*html*/`<p>De copa larga, a <i>Araucaria angustifólia</i>, conhecida apenas como Araucária, pode atingir até 50 metros de altura. É um gênero de árvore dióica, ou seja, que possui sexos separados. A Araucária fêmea é caracterizada pela pinha e o macho pelo estróbilo. Quando o estróbilo amadurece libera o pólen que se dispersa pelo vento e fertiliza a pinha. Após dois anos, as sementes amadurecem e é quando ocorre o desprendimento dos grãos (sementes), conhecidos como pinhões. Eles são importantes para a manutenção da biodiversidade, servindo de alimento para diversos animais como o serelepe, a gralha-do-campo e também os seres humanos.</p> 
+  <p>
+    Devido à exploração, a Araucária está na lista de flora brasileira ameaçada de extinção. O reflorestamento, a partir das mudas, é uma ação estratégica de preservação da espécie, da biodiversidade e da minimização dos efeitos das mudanças climáticas.
+     A Araucária é árvore-símbolo de Campos do Jordão e, no Museu Felícia Leirner, elas estão nos jardins, entre as alamedas, ao lado de esculturas e na mata ao redor.
+  </p> `],
+
+  2: ['SUCUPIRA',
+  /*html*/`<p>A Sucupira, <i>Pterodon emarginatus</i>, é uma árvore nativa da Mata Atlântica e tem o tronco revestido por uma casca com ritidoma com uma textura semelhante à de uma rolha. Essa casca protege os tecidos do excesso de evaporação e do contato com agentes nocivos presentes no ambiente. Além disso, sua raiz pode formar expansões que são conhecidas como “batata-de-sucupira” que atuam como áreas de reserva da planta. A Sucupira pode ser encontrada nos jardins do Museu Felícia Leirner, próximo à “Fase Orgânica”.</p>`],
+
+  3: ['AGARICUS',
+   /*html*/`<p><i>Agaricus</i> é um importante gênero de cogumelos. Com tamanho médio, chapéu hemisférico, pé cilíndrico e cor esbranquiçada, essa categoria possui espécies comestíveis e venenosas. No Museu, eles estão nos jardins, tendo ocorrência próximo às hortênsias a caminho da bilheteria. </p> 
+  `],
+
+  4: ['PAINEIRA',
+  /*html*/`<p>
+    Da família <i>Malvaceae</i>, a Paineira é uma árvore nativa que pode chegar até 30 metros de altura. No museu, ela está presente no início do percurso cronológico, próximo às esculturas em bronze pertencentes à fase “Figurativa”.
+    Seu tronco possui acúleos, popularmente chamados de espinhos, que contrastam com a delicadeza e beleza das flores cor-de-rosa comuns entre o verão e o início do outono. Os acúleos atuam como proteção, afastando as ameaças.
+  </p>
+  <p>Os frutos assemelham-se com cápsulas que liberam sementes utilizadas para a produção de novas mudas disponibilizadas para ações de reflorestamento e preservação ambiental. </p>`],
+
+  5: ['LANGSDORFFIA HYPOGAEA',
+   /*html*/`<p><i>Langsdorffia hypogaea</i> é uma planta parasita encontrada na floresta ombrófila que cerca os jardins do Museu Felícia Leirner. Elas possuem tubérculos subterrâneos e talos com folhas escamosas. É utilizada na confecção de velas no centro-oeste brasileiro.</p> `],
+
+  6: ['PIXIRICA', /*html*/`<p>A Pixirica, ou <i>Clidemia hirta</i>, é um arbusto que cresce até dois metros de altura. Possui frutos pequenos na cor azul escuro e arroxeados. Suas folhas têm uso medicinal, enquanto seu fruto pode ser consumido pela fauna local e também pelos seres humanos. Na época de floração, ela atrai abelhas para a polinização. No Museu, estão próximas às escadas que dão acesso ao mirante.</p> `],
+
+  7: ['MANACÁ DA SERRA',
+  /*html*/`<p>O Manacá da Serra, <i>Tibouchina mutabilis</i>, é uma espécie nativa da Mata Atlântica que pode chegar a até 12 metros de altura. Seu nome, <i>mutabilis</i>, significa mutável, que justifica o fato de suas flores mudarem de cor. Nas cores branca e rosa, as pétalas contrastam com o verde-escuro das folhas que são rijas e pilosas.</p> 
+  <p>
+    Ela floresce entre os meses de novembro e fevereiro, estendendo-se em algumas regiões. Tem um apelo paisagístico pelo porte pequeno e por suas raízes não agressivas ao calçamento. O Manacá é muito útil e importante para o reflorestamento de áreas de preservação.
+    No Museu Felícia Leirner é possível encontrá-lo nos jardins, entre as alamedas e próximo às esculturas em cimento armado. O Manacá da Serra é ainda uma das espécies presentes no viveiro de mudas da instituição.
+  </p>`],
+
+  8: ['CHROMOLAENA',
+  /*html*/`<p><i>Chromolaena</i> é um gênero pertencente à família Asteraceae. Está presente próximo ao mirante do Museu Felícia Leirner. Suas flores na cor lilás contrastam com as folhas verdes ovaladas, atraindo insetos como borboletas e abelhas.</p> `],
+
+  9: ['HYDNOPOLYPORUS',
+   /*html*/`<p><i>Hydnopolyporus</i> é um gênero de fungo pertencente à família <i>Meripilaceae</i> que participa ativamente na ciclagem de nutrientes e na decomposição. No Museu Felícia Leirner é possível encontrá-los em raízes expostas e no tronco das árvores.</p> `],
+
+  10: ['SAMAMBAIAÇU',
+  /*html*/`<p>Nativa da Mata Atlântica, Samambaiaçu, também conhecida por xaxim, possui caule ereto e ramos que chegam a até dois metros. A incidência dessa espécie, <i>Dicksonia sellowiana</i>, se dá em região de sombra ou meia-sombra, em terrenos úmidos e ricos em matéria orgânica, servindo de suporte para plantas epífitas como bromélias e orquídeas. </p>
+  <p>
+    O tronco da Samambaiaçu dá origem a substratos e vasos utilizados nos cultivos de diversas plantas. Devido à exploração comercial, entrou para a lista de ameaçados de extinção, sendo assim, a extração e a industrialização do xaxim são, hoje, proibidas por lei.
+    A Samambaiaçu se multiplica por meio de esporos e seu crescimento é lento, cerca de cinco a oito centímetros por ano. No Museu Felícia Leirner elas estão presentes na mata e nos jardins de esculturas, dentro das áreas sombreadas.
+  </p> `],
+
+  11: ['MIMOSA PUDICA',
+  /*html*/`<p>Com flores que parecem pompons, as dormideiras, <i>Mimosa pudica</i>, se desenvolvem em áreas de meia-sombra ou sol pleno. Quando tocadas, fecham suas folhas como estratégia natural de defesa. Elas podem ser encontradas próximas ao viveiro de mudas do Museu Felícia Leirner. </p>`],
+
+  12: ['AECHMEA DISTICHANTHA',
+  /*html*/`<p><i>Aechmea distichantha</i> é uma espécie de bromélia nativa de porte médio que atrai abelhas e pássaros para polinização. Suas flores possuem pétalas azuis, brancas e rosadas. São plantas epífitas que se desenvolvem sobre outras sem causar nenhum prejuízo. No Museu Felícia Leirner podemos encontrá-las nos galhos e troncos das araucárias.</p> `],
+
+  13: ['BRINCO-DE-PRINCESA',
+   /*html*/`<p>
+     O brinco-de-princesa é uma espécie híbrida do gênero <i>fuchsia</i>. Ela é um arbusto perene que pode atingir até três metros de altura. Sua incidência ocorre na América do Sul, havendo cerca de 200 espécies diferentes da planta. Possui variedades de pétalas, cores e formas, sendo possível encontrá-las nas cores rosa, violeta, branca e vermelha. 
+       A planta se adapta com facilidade aos ambientes podendo ser cultivada de forma ornamental. Suporta o frio e a geada que caracterizam o inverno jordanense. A floração do brinco-de-princesa ocorre quase o ano todo, atraindo beija-flores e outros polinizadores. Sendo assim, esta espécie é importante para a manutenção da biodiversidade.
+   </p>
+  <p>No Museu Felícia Leirner, elas estão presentes nos jardins e próximas às alamedas, colorindo a paisagem e desempenhando um importante papel de preservação da mata ombrófila. </p>`],
+
+  14: ['ONCIDIUM SP',
+  /*html*/`<p>Conhecida como chuva-de-ouro, a <i>Oncidium sp</i> é uma espécie de orquídea que apresenta flores amarelas, marrons e alaranjadas. As pétalas são bem pequenas, medindo dois centímetros e atraem borboletas. No Museu, elas podem ser encontradas perto das escadas a caminho do mirante. </p>`],
+
+  15: ['ABELIA',
+  /*html*/`<p><i>Abelia grandiflora</i> é um arbusto que floresce entre o verão e o outono. Suas folhas são pequenas e as flores são produzidas em cachos brancos e rosa, tendo cerca de dois centímetros. Preferem solos úmidos e ricos em matéria orgânica. No Museu Felícia Leirner estão nas alamedas e escadas que dão acesso à concha acústica.</p> `],
+
+  16: ['QUARESMEIRA',
+   /*html*/`<p>
+     A quaresmeira, <i>Tibouchina granulosa</i>, pertencente à família <i>Melastomataceae</i>, é uma árvore nativa da Mata Atlântica que pode atingir até 12 metros de altura. Suas folhas são rijas e cruzadas, suas flores são vistosas, na cor roxa. A floração ocorre duas vezes por ano, no outono e na primavera. 
+       É uma espécie pioneira, desempenhando um papel fundamental na recuperação de áreas desmatadas, sendo útil para o reflorestamento, recuperação da flora e servindo de suporte para a fauna local.
+   </p>
+  <p>No Museu Felícia Leirner é possível encontrá-la nos jardins, próximos às alamedas e esculturas.</p>`]
+}
+
 const eachSlide = img =>
   /*html*/`<div class="general-container" id="${img}">
 <div class="general-container-inner">
@@ -8,8 +82,8 @@ const eachSlide = img =>
   </div>
   <div class="container-description">
   <div class="container-description-inner">
-  <p><cite>Photo ${img}</cite> - John Doe </p>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias qui aliquid a amet voluptates repudiandae!</p>
+  <p><cite>${conteudo[img][0]}</p>
+ ${conteudo[img][1]}
   </div>
   </div>
 </div>
@@ -21,8 +95,8 @@ const eachSlideAlt = img =>  /*html*/`<div class="general-container split" id="$
   </div>
   <div class="container-text">
   <div class="container-text-inner">
-  <p><cite>Photo ${img}</cite> - John Doe </p>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, illo illum soluta totam reiciendis hic amet dolorum et voluptates provident, iste ab rerum sequi unde. Corrupti est temporibus possimus dolorum, ad, ratione nihil ex nemo placeat tenetur id exercitationem ut voluptatem a ipsum dolore. Aliquam ducimus eveniet at molestias possimus.</p>
+  <p><cite>${conteudo[img][0]}</p>
+  ${conteudo[img][1]}
   </div>
   </div>
   <div class="container-img">
@@ -33,57 +107,74 @@ const eachSlideAlt = img =>  /*html*/`<div class="general-container split" id="$
 
 const finalSlide = () =>  /*html*/`<div class="general-container final" id="final">
 <div class="general-container-inner ">
-<a class="go-back" href="#">Back</a>
+<a class="go-back" href="https://museufelicialeirner.org.br">Voltar</a>
   <div class="bg" >
   </div>
   <div class="container-content-final">
-   <h2>Lorem ipsum</h2>
+   <h2>Ficha Técnica</h2>
    <div class="row-of-credits">
-   <ul>
-   <span>Lorem Ipsum</span>
-   <li>Lorena Ipsum</li>
-   <li>Lorenzo Ipsum</li>
-   <li>Loiane Ipsum</li>
+   <ul class="area1">
+   <span>GOVERNO DO ESTADO DE SÃO PAULO</span>
+   <li><strong>Rodrigo Garcia</strong><br/>Governador do estado</li>
+   <li><strong>Sérgio Sá Leitão</strong><br/>Secretário de Cultura e Economia Criativa</li>
+   <li><strong>Cláudia Pedrozo</strong><br/>
+   Secretária Executiva de Cultura e Economia Criativa</li>
+   <li><strong>Frederico Mascarenhas</strong><br/>
+   Chefe de Gabinete de Cultura e Economia Criativa</li>
+   <li><strong>Paula Paiva Ferreira</strong><br/>
+   Coordenadora da Unidade de Preservação do Patrimônio Museológico – UPPM</li>
    </ul>
-   <ul>
-   <span>Lorem Ipsum</span>
-   <li>Lorena Ipsum</li>
-   <li>Lorenzo Ipsum</li>
-   <li>Loiane Ipsum</li>
+
+   <ul class="area2">
+   <span>ACAM PORTINARI - ORGANIZAÇÃO SOCIAL DE CULTURA</span>
+   <li><strong>Paulo Augusto Coelho de Souza</strong><br/>
+   Presidente do Conselho Administrativo</li>
+   <li><strong>Angelica Fabbri</strong><br/>
+   Diretora Executiva</li>
+   <li><strong>Luiz Antonio Bergamo</strong><br/>
+   Diretor Administrativo Financeiro</li>
    </ul>
-   <ul>
-   <span>Lorem Ipsum</span>
-   <li>Lorena Ipsum</li>
-   <li>Lorenzo Ipsum</li>
-   <li>Loiane Ipsum</li>
+
+   <ul class="area3">
+   <span>MUSEU FELÍCIA LEIRNER E AUDITÓRIO CLAUDIO SANTORO – CAMPOS DO JORDÃO</span>
+   <li><strong>Frederico David de Souza</strong><br/>
+   Gerente de Unidade</li>
+   <li><strong>Educativo</strong><br/>
+   Ana Clara Atanazio Cunha<br/>
+   Andreza Lima Damião<br/>
+   Leandra Fernandes Lucio </li>
+   <li><strong>Administrativo</strong><br/>
+   Adina Iracema Cesarino | Assistente Administrativo<br/>
+   Isabel Cristina dos Santos | Assistente Administrativo</li>
+   <li><strong>Programação</strong><br/>
+   Laila Cristina Vils Alves De Souza</li>
+   <li><strong>Acervo</strong><br/>
+   Letícia De Mello Menezes</li>
+   <li><strong>Manutenção</strong><br/>
+   Fabrício Barbosa dos Santos | Ajudante de Manutenção<br/>
+   Weverton Candido Ferreira | Manutenção Predial</li>
+   <li><strong>Comunicação</strong><br/>
+   Juliana Faria da Silva | Assistente de Comunicação
+   </li>
+   <li><strong>Estagiários</strong><br/>
+   Bianca Almeida de Lima | Estagiária Educativo<br/>
+   Laura Giglio dos Santos | Estagiária de Programação<br/>
+   Vanessa Cristina Silva e Silva | Estagiária Educativo<br/>
+   Vitória Isabeli Rafael de Castro | Estagiária Educativo</li>
    </ul>
-   <ul>
-   <span>Lorem Ipsum</span>
-   <li>Lorena Ipsum</li>
-   <li>Lorenzo Ipsum</li>
-   <li>Loiane Ipsum</li>
-   </ul>
-   <ul>
-   <span>Lorem Ipsum</span>
-   <li>Lorena Ipsum</li>
-   <li>Lorenzo Ipsum</li>
-   <li>Loiane Ipsum</li>
-   </ul>
-   <ul>
-   <span>Lorem Ipsum</span>
-   <li>Lorena Ipsum</li>
-   <li>Lorenzo Ipsum</li>
-   <li>Loiane Ipsum</li>
+
+   <ul class="area4">
+   <span>EXPOSIÇÃO VIRTUAL</span>
+   <li><strong>Fotografia</strong><br/>
+   Juliana Faria da Silva</li>
+   <li><strong>Texto e Pesquisa</strong><br/>
+   Andreza Lima Damião<br/>
+   Juliana Faria da Silva</li>
    </ul>
    </div>
    <div class="footer">
    <div>
-   <img src="./img/logoipsum-logo-8.svg" alt="" />
-   <img src="./img/logoipsum-logo-10.svg" alt="" />
-   <img src="./img/logoipsum-logo-22.svg" alt="" />
-   </div>
-   <div>
-   <p>© Lorem ipsum dolor</p>
+   <p>© Museu Felícia Leirner 2022</p>
    </div>
    </div>
   </div>
@@ -91,10 +182,11 @@ const finalSlide = () =>  /*html*/`<div class="general-container final" id="fina
 </div>`
 
 let element = ""
-const slideCount = 10
+const slideCount = 16
+const slidesAlt = [1, 4, 7, 10, 13, 16]
 
 for (let i = 1; i <= slideCount; i++) {
-  if (i === 2) {
+  if (slidesAlt.includes(i)) {
     element += eachSlideAlt(i)
   } else {
     element += eachSlide(i)
